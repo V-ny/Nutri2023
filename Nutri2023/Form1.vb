7def99179b97421774bea203ec6305b0555d01ee
Vinicius Aguiar <vinibm@outlook.com>
2025-02-11 13:16:38 -0300

    Sistema de gerenciamento clinico

---
+++ Nutri2023/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub PACIENTESBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles PACIENTESBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.PACIENTESBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.NUTRI2023DataSet)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: esta linha de código carrega dados na tabela 'NUTRI2023DataSet.PACIENTES'. Você pode movê-la ou removê-la conforme necessário.
+        Me.PACIENTESTableAdapter.Fill(Me.NUTRI2023DataSet.PACIENTES)
+
+    End Sub
+End Class
